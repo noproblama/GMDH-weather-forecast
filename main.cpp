7a42f9291f008c4b290ffbbac07368cc56bfe9cf
WhoseSomebody <whosesomebody@gmail.com>
2017-03-20 00:39:44 +0200

    Integrated for one proccessor. Using real data from file. Comparing via sqr division.

--- main.cpp
+++ main.cpp
@@ -4,18 +4,24 @@
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
-vector<int> DEFAULT_COEFF_B(7, 1);
+vector<double> DEFAULT_COEFF_B(7, 1);
 
 vector<double> findVectorOfCoefB(const vector<int> &, const vector<double> &, const vector<double> &);
 vector<vector<double> > transpose(const vector<vector<double> > &);
 vector<vector<double> > operator*(const vector<vector<double> >& a, const vector<vector<double> >& b);
-vector<double> calculate(const vector<int>&, double, const vector<int>& = DEFAULT_COEFF_B);
+vector<double> calculate(const vector<int>&, double, const vector<double>& = DEFAULT_COEFF_B);
 
 double CalcDeterminant(vector<vector<double> > , int);
 vector<vector<double> > GetMinor(vector<vector<double> >, int, int, int);
-vector<vector<double> > MatrixInversion(vector<vector<double> >);
+vector<vector<double> > inverse(vector<vector<double> >);
+string binary(unsigned);
+double squareComparation(const vector<int>&, const vector<double>&, const vector<double>&, const vector<double>&);
+
 
 int main(int argc, char * argv[]) {
     int numtasks, taskid;
@@ -37,35 +43,99 @@ int main(int argc, char * argv[]) {
     }
 
     double t = 0.0;
-    double indxCounter = 0.0;
+    int indxCounter = 0;
     while(ifile >> t) {
         indxCounter++;
-        if(yA.size() > yB.size()) {
+        if(yA.size() < yB.size()) {
             yA.push_back(t);
-            xA.push_back(indxCounter);
+            xA.push_back((double)indxCounter);
         } else {
             yB.push_back(t);
-            xB.push_back(indxCounter);
+            xB.push_back((double)indxCounter);
         }
     }
+            // cout << yA.size() << " ";
 
-   vector<vector<double> > r1(5, vector<double>(3, 1));
-   vector<vector<double> > r2(3, vector<double>(7, 1));
-   vector<vector<double> > r3(7, vector<double>(3, 1));
+            // cout << yB.size() << " ";
+
+
+    // for (int i = 0; i < yB.size(); ++i)
+    // {
+    //     cout << xB[i] << " " << yB[i] << endl ;
+    // }
+
+
+   // vector<vector<double> > r1(5, vector<double>(3, 1));
+   // vector<vector<double> > r2(3, vector<double>(7, 1));
+   // vector<vector<double> > r3(7, vector<double>(3, 1));
    //(r1*r2)*r3;
    
     //cout << firstT.size() << secondT.size()<< endl;
 
-    int test_l[] = {1, 6};
-    double text_xa[] = {1.0, 1.2, 1.3, 1.4, 1.5};
-    double text_ya[] = {1.1, 1.2, 1.3, 1.4, 1.5};
+    // double text_xa[] = {1.0, 1.2, 1.3, 1.4, 1.5};
+    // double text_ya[] = {1.1, 1.2, 1.3, 1.4, 1.5};
 
-    vector<int> L(test_l, test_l + sizeof(test_l) / sizeof(int));
-    vector<double> x(text_xa, text_xa + sizeof(text_xa) / sizeof(double));
-    vector<double> y(text_ya, text_ya + sizeof(text_ya) / sizeof(double));
+    // vector<double> x(text_xa, text_xa + sizeof(text_xa) / sizeof(double));
+    // vector<double> y(text_ya, text_ya + sizeof(text_ya) / sizeof(double));
 
 
-    vector<double> b (findVectorOfCoefB(L, x, y));
+
+
+    vector <vector<int> > L;
+    for (int i = 1; i < 64; ++i)
+    {
+        L.push_back(vector<int>(1,1));
+        string binary_number = binary(i);
+        for (int j = 0; j < binary_number.size(); ++j) {
+            if (binary_number.compare(j,1,"1") == 0) {
+                L[i-1].push_back(j + 2);
+            }
+        }
+    }
+
+    vector<vector<double> > Bs;
+    for (int i = 0; i < L.size(); ++i)
+    {
+        Bs.push_back(vector<double> (findVectorOfCoefB(L[i], xA, yA)));  
+
+        // for (int j = 0; j < Bs.size(); ++j)
+        // {
+        //     cout << Bs[i][j] << " ";
+
+        // }
+        // cout << endl; 
+    }
+
+    vector<double> square_comparations;
+    for (int i = 0; i < Bs.size(); ++i)
+    {
+        square_comparations.push_back(squareComparation(L[i], Bs[i], xB, yB));
+    }
+    
+    // for (int i = 0; i < square_comparations.size(); ++i)
+    // {
+    //     cout << square_comparations[i] << " ";
+    // }
+
+    // cout << endl;
+
+    int min_pos = distance(square_comparations.begin(),min_element(square_comparations.begin(),square_comparations.end()));
+    cout << "The best model is the model #" << min_pos + 1 << " (" << square_comparations[min_pos] << ") "<<" with b: ";
+    for (int j = 0; j < Bs[min_pos].size(); ++j)
+    {
+        cout << Bs[min_pos][j] << " ";
+
+    }
+    cout << endl; 
+
+
+    // for (int j = 0; j < b.size(); ++j)
+    // {
+    //     cout << b[j] << " ";
+
+    // }
+    // cout << endl;
+
 
     MPI_Finalize();
 }
@@ -84,43 +154,46 @@ vector<double> findVectorOfCoefB(const vector<int> &L, const vector<double> &XA,
         X_functions.push_back(calculate(L, XA[i]));
     }                    
 
-    cout << "COUNTED ONE" << endl;
-    for(int i = 0; i < X_functions.size(); i++){
-        for (int j = 0; j < X_functions[0].size(); ++j)
-        {
-            cout << X_functions[i][j] << " ";
+    // cout << "COUNTED ONE" << endl;
+    // for(int i = 0; i < X_functions.size(); i++){
+    //     for (int j = 0; j < X_functions[0].size(); ++j)
+    //     {
+    //         cout << X_functions[i][j] << " ";
     
-        }
-        cout << endl;
-    }
-    cout << endl;
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
 
     X_transposed = transpose(X_functions);
 
-    cout << "TRANSPOSED ONE" << endl;
-    for(int i = 0; i < X_transposed.size(); i++){
-        for (int j = 0; j < X_transposed[0].size(); ++j)
-        {
-            cout << X_transposed[i][j] << " ";
+    // cout << "TRANSPOSED ONE" << endl;
+    // for(int i = 0; i < X_transposed.size(); i++){
+    //     for (int j = 0; j < X_transposed[0].size(); ++j)
+    //     {
+    //         cout << X_transposed[i][j] << " ";
     
-        }
-        cout << endl;
-    }
+    //     }
+    //     cout << endl;
+    // }
 
     Y = transpose(YT);
-    vector<double> calculated_bs (transpose((((X_transposed*X_functions)*X_transposed)*Y))[0]);
+
+    // vector<vector<double> > hui, pizda, pizda_I, upyachka, upyachka_T;
+    // hui = X_transposed*X_functions;
+    // pizda = hui*X_transposed;
+    // pizda_I = inverse(pizda);
+    // upyachka = pizda_I*X_transposed;
+    // upyachka_T = transpose(upyachka);
+
+    // cout << "SIZE X: " << X_functions.size() << "x" << X_functions[0],size() << endl;
+    // cout << "SIZE XT: " << X_transposed.size() << "x" << X_transposed[0],size() << endl;
+    // cout << "SIZE (X*XT)^-1: " << hui.size() << "x" << hui[0],size() << endl;
+    // cout << "SIZE X*XT: " << hui.size() << "x" << hui[0],size() << endl;
+    vector<double> calculated_bs (transpose((inverse(X_transposed*X_functions)*X_transposed)*Y)[0]);
 
     // cout<< ((X_transposed*X_functions)*X_transposed) << endl;
     // cout<< Y[0].size() << endl;
-
-    // cout << "bs calculated:" << endl;
-
-    // for (int j = 0; j < calculated_bs.size(); ++j)
-    // {
-    //     cout << calculated_bs[j] << " ";
-
-    // }
-    // cout << endl;
 
     return calculated_bs;
 }
@@ -139,7 +212,7 @@ vector<vector<double> > transpose(const vector<vector<double> > &matrix) {
     return transposedMatrix;
 };
 
-vector<double> calculate(const vector<int>& indxFunc, double x, const vector<int>& b) {
+vector<double> calculate(const vector<int>& indxFunc, double x, const vector<double>& b) {
 
     vector<double> calculatedVal;
 
@@ -150,27 +223,27 @@ vector<double> calculate(const vector<int>& indxFunc, double x, const vector<int
             break;
             
             case 2:
-                calculatedVal.push_back(b[1] * sin(4 * x));
+                calculatedVal.push_back(b[1] * sin(2 * 3.14 * x / 365));
             break;
 
             case 3:
-               calculatedVal.push_back(b[2] * tan(7 * x));
+               calculatedVal.push_back(b[2] * cos(2 * 3.14 * x / 365));
             break;
 
             case 4:
-                 calculatedVal.push_back(b[3] * cos(12 * x));
+                 calculatedVal.push_back(b[3] * sin(24 * 3.14 * x / 365));
             break;
 
             case 5:
-                 calculatedVal.push_back(b[4] * (1 / tan(7 * x)));
+                 calculatedVal.push_back(b[4] * cos(24 * 3.14 * x / 365));
             break;
 
             case 6:
-                calculatedVal.push_back(b[5] * sin(7 * x));
+                calculatedVal.push_back(b[5] * sin(12 * 3.14 * x / 365));
             break;
 
             case 7:
-                calculatedVal.push_back(b[6] * cos(4 * x));
+                calculatedVal.push_back(b[6] * cos(12 * 3.14 * x / 365));
             break;
 
             default:
@@ -217,7 +290,7 @@ vector<vector<double> > operator*(const vector<vector<double> >& a, const vector
 }
 
 // matrix inversioon
-vector<vector<double> > MatrixInversion(vector<vector<double> > matrix)
+vector<vector<double> > inverse(vector<vector<double> > matrix)
 {
     
     int order = matrix.size();
@@ -298,4 +371,42 @@ double CalcDeterminant(vector<vector<double> > matrix, int order)
 
  
     return det;
+}
+
+// Convert int to binary string
+string binary(unsigned x)
+{
+    string s;
+    do
+    {
+        s.push_back('0' + (x & 1));
+    } while (x >>= 1);
+
+    reverse(s.begin(), s.end());
+    for (int i = s.size(); i < 6; i++) {
+        s.insert(s.begin(),'0');
+       
+    }
+    return s;
+
+}
+
+double squareComparation(const vector<int> &L, const vector<double> &b, const vector<double> &xB, const vector<double> &yB) {
+    double  result,
+            model_minus_table_sqr_sum,
+            table_sqr_sum;
+
+    for (int i = 0; i < xB.size(); ++i)
+    {
+        vector<double> model_res_vector = calculate(L, xB[i], b);
+        double  model_res = accumulate(model_res_vector.begin(), model_res_vector.end(), 0);
+        double  table_res = yB[i];
+
+        model_minus_table_sqr_sum += pow(model_res-table_res, 2);
+        table_sqr_sum += pow(table_res, 2);
+    }
+
+    result = model_minus_table_sqr_sum / table_sqr_sum;
+    
+    return result;
 }
