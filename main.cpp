3ca97c971a849a350b2d120450cf5549e27fb0ed
SerG-Y <serg.yaschuk@gmail.com>
2017-03-19 18:06:45 +0200

    added calculate function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mpi.h"
 #include <fstream>
 #include <vector>
+#include <cmath>
 #include <iostream>
 using namespace std;
 
@@ -8,6 +9,8 @@ vactor<double> findVectorOfCoefB(vector<int> &, vector<double> &, vector<double>
 vector<vector<double>> transpose(vector<vector<double>> &);
 vector<vector<double>> multiply(vector<vector<double>> &, vector<vector<double>> &);
 
+vector<int> DEFAULT_COEFF_B(7, 1);
+vector<double> calculate(const vector<int>&, double, const vector<int>& = DEFAULT_COEFF_B);
 
 int main(int argc, char * argv[]) {
     int numtasks, taskid;
@@ -16,9 +19,11 @@ int main(int argc, char * argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks); 
 	MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
 
-    vector<double> firstT;
-    vector<double> secondT;
-
+    vector<double> yA;
+    vector<double> xA;
+    vector<double> yB;
+    vector<double> xB;
+    
     ifstream ifile("data.txt");
 
     if (!ifile.is_open()) {
@@ -27,16 +32,21 @@ int main(int argc, char * argv[]) {
     }
 
     double t = 0.0;
+    double indxCounter = 0.0;
     while(ifile >> t) {
-        if(firstT.size() > secondT.size())
-            secondT.push_back(t);
-        else 
-            firstT.push_back(t);
+        indxCounter++;
+        if(yA.size() > yB.size()) {
+            yA.push_back(t);
+            xA.push_back(indxCounter);
+        } else {
+            yB.push_back(t);
+            xB.push_back(indxCounter);
+        }
     }
 
-   
-    std::cout << firstT.size() << secondT.size()<< std::endl;
-
+   vector<int> s(5,4);
+    //cout << firstT.size() << secondT.size()<< endl;
+    calculate(s, 2);
     MPI_Finalize();
 }
 
@@ -77,4 +87,51 @@ vector<vector<double>> transpose(vector<vector<double>> &matrix) {
 vector<vector<double>> multiply(vector<vector<double>> &matrixA, vector<vector<double>> &matrixB) {
     vector<vector<double>> multipliedM;
     return multipliedM;
+}
+
+vector<double> calculate(const vector<int>& indxFunc, double x, const vector<int>& b) {
+
+    vector<double> calculatedVal;
+
+    for(int i = 0; i < indxFunc.size(); i++) {
+        switch(indxFunc[i]) {
+            case 1:
+                calculatedVal.push_back(b[0] * 1);
+            break;
+            
+            case 2:
+                calculatedVal.push_back(b[1] * sin(4 * x));
+            break;
+
+            case 3:
+               calculatedVal.push_back(b[2] * tan(7 * x));
+            break;
+
+            case 4:
+                 calculatedVal.push_back(b[3] * cos(12 * x));
+            break;
+
+            case 5:
+                 calculatedVal.push_back(b[4] * (1 / tan(7 * x)));
+            break;
+
+            case 6:
+                calculatedVal.push_back(b[5] * sin(7 * x));
+            break;
+
+            case 7:
+                calculatedVal.push_back(b[6] * cos(4 * x));
+            break;
+
+            default:
+            break;
+        }
+    }
+
+    // for(int i = 0; i < calculatedVal.size(); i++)
+    //     cout << calculatedVal[i] << " ";
+
+    // cout << endl;
+
+    return calculatedVal;
 }
